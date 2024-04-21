49e6323112958f1571345dc41647de840456aec2
Gabriele Varchetta <gabrielevarch@gmail.com>
2024-04-21 17:30:08 +0200

    Play interaction name change

--- events/player/playSong.js
+++ events/player/playSong.js
@@ -6,20 +6,25 @@ module.exports = async (client, queue, song) => {
     if (!client.config.opt.loopMessage && queue?.repeatMode !== 0) return;
     if (queue?.textChannel) {
       const embed = new EmbedBuilder()
-      .setAuthor({
-        name: 'Currently playing a Track',
-        iconURL: 'https://cdn.discordapp.com/attachments/1140841446228897932/1144671132948103208/giphy.gif', 
-        url: 'https://discord.gg/FUEHs7RCqz'
-    })
-    .setDescription(`\n ‎ \n▶️ **Details :** **${song?.name}**\n▶️ **Enjoy the Ultimate Music Experience. ** \n▶️ **If link breaks playback try to give query.**`)
-.setImage(queue.songs[0].thumbnail)
-    .setColor('#FF0000')
-    .setFooter({ text: 'More info - Use /help command [GlaceYT]' });
-     
-      queue?.textChannel?.send({ embeds: [embed] }).catch(e => { });
+        .setAuthor({
+          name: "Vai Flavié mannala",
+          iconURL:
+            "https://cdn.discordapp.com/attachments/1140841446228897932/1144671132948103208/giphy.gif",
+          url: "https://discord.gg/FUEHs7RCqz",
+        })
+        .setDescription(
+          `\n ‎ \n▶️ **Details :** **${song?.name}**\n▶️ **Enjoy the Ultimate Music Experience. ** \n▶️ **If link breaks playback try to give query.**`
+        )
+        .setImage(queue.songs[0].thumbnail)
+        .setColor("#FF0000")
+        .setFooter({
+          text: "More info - Use /help command PapiChuloSama (codice non rubato)",
+        });
+
+      queue?.textChannel?.send({ embeds: [embed] }).catch((e) => {});
     }
   }
-}
+};
 
 /*
 
